110d474ae77a72d74b075874b3242c57a55b6314
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-14 14:31:39 +0300

    part 1:src/main/java/com/patika/kredinbizdenservice/model/Loan/LoanFactory.java

--- src/main/java/com/patika/kredinbizdenservice/model/Loan/LoanFactory.java
+++ src/main/java/com/patika/kredinbizdenservice/model/Loan/LoanFactory.java
@@ -1,4 +1,20 @@
 package com.patika.kredinbizdenservice.model.Loan;
 
+import com.patika.kredinbizdenservice.enums.LoanType;
+
 public class LoanFactory {
+
+    public static Loan createLoan(LoanType loanType) {
+        switch (loanType) {
+            case IHTIYAC_KREDISI:
+                return ConsumerLoan.createRandom();
+            case KONUT_KREDISI:
+                return HouseLoan.createRandom();
+            case ARAC_KREDISI:
+                return VehicleLoan.createRandom();
+            default:
+                throw new IllegalArgumentException("Unexpected value: " + loanType);
+        }
+    }
+
 }
