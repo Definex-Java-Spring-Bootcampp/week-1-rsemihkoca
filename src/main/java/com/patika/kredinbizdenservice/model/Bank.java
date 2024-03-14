96068fb20c0250c995a626b6eea3e10d5529c94f
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-14 14:31:39 +0300

    part 1:src/main/java/com/patika/kredinbizdenservice/model/Bank.java

--- src/main/java/com/patika/kredinbizdenservice/model/Bank.java
+++ src/main/java/com/patika/kredinbizdenservice/model/Bank.java
@@ -1,34 +1,57 @@
 package com.patika.kredinbizdenservice.model;
 
+import com.patika.kredinbizdenservice.model.Loan.Loan;
+import lombok.Getter;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Bank {
 
     private String name;
-    private List<Loan> loanList;
-    private List<CreditCard> creditCards;
+    private String location;
+    private static List<Loan> loanList = new ArrayList<>();
+    @Getter
+    private static List<CreditCard> creditCards = new ArrayList<>();
 
-    public String getName() {
-        return name;
+    public static void addCreditCard(CreditCard creditCard) {
+        creditCards.add(creditCard);
     }
 
-    public void setName(String name) {
+    public Bank(String name, String location) {
         this.name = name;
+        this.location = location;
     }
 
-    public List<Loan> getLoanList() {
-        return loanList;
+    public static void addLoan(Loan loan) {
+        loanList.add(loan);
     }
 
-    public void setLoanList(List<Loan> loanList) {
-        this.loanList = loanList;
-    }
-
-    @Override
     public String toString() {
         return "Bank{" +
                 "name='" + name + '\'' +
-                ", loanList=" + loanList +
+//                ", loanList=" + loanList +
                 '}';
+    }
+
+    public static Bank createRandom() {
+        Random random = new Random();
+        List<Bank> banks = List.of(
+                new Bank("Garanti", "Istanbul"),
+                new Bank("Akbank", "Istanbul"),
+                new Bank("Yapı Kredi", "Istanbul"),
+                new Bank("Ziraat", "Ankara"),
+                new Bank("Vakıf", "Ankara"),
+                new Bank("Halk", "Ankara"),
+                new Bank("TEB", "Istanbul"),
+                new Bank("ING", "Istanbul"),
+                new Bank("QNB Finansbank", "Istanbul"),
+                new Bank("Denizbank", "Istanbul")
+        );
+
+        return banks.get(random.nextInt(banks.size()));
+
+
     }
 }
