ee5111d043974cfb2b4e2fa2ae00b6e85c57f9f7
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-16 01:59:17 +0300

    factory and singleton implemented:src/main/java/com/patika/kredinbizdenservice/model/Bank.java

--- src/main/java/com/patika/kredinbizdenservice/model/Bank.java
+++ src/main/java/com/patika/kredinbizdenservice/model/Bank.java
@@ -1,7 +1,6 @@
 package com.patika.kredinbizdenservice.model;
 
 import com.patika.kredinbizdenservice.model.Loan.Loan;
-import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,20 +11,10 @@ public class Bank {
     private String name;
     private String location;
     private static List<Loan> loanList = new ArrayList<>();
-    @Getter
-    private static List<CreditCard> creditCards = new ArrayList<>();
 
-    public static void addCreditCard(CreditCard creditCard) {
-        creditCards.add(creditCard);
-    }
-
-    public Bank(String name, String location) {
+    private Bank(String name, String location) {
         this.name = name;
         this.location = location;
-    }
-
-    public static void addLoan(Loan loan) {
-        loanList.add(loan);
     }
 
     public String toString() {
@@ -40,14 +29,8 @@ public class Bank {
         List<Bank> banks = List.of(
                 new Bank("Garanti", "Istanbul"),
                 new Bank("Akbank", "Istanbul"),
-                new Bank("Yapı Kredi", "Istanbul"),
-                new Bank("Ziraat", "Ankara"),
-                new Bank("Vakıf", "Ankara"),
-                new Bank("Halk", "Ankara"),
                 new Bank("TEB", "Istanbul"),
-                new Bank("ING", "Istanbul"),
-                new Bank("QNB Finansbank", "Istanbul"),
-                new Bank("Denizbank", "Istanbul")
+                new Bank("Isbank", "Istanbul")
         );
 
         return banks.get(random.nextInt(banks.size()));
