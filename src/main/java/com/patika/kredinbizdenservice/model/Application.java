f4c43ca91c4e6a4c006d19336fa54196c719cde1
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-16 01:59:17 +0300

    factory and singleton implemented:src/main/java/com/patika/kredinbizdenservice/model/Application.java

--- src/main/java/com/patika/kredinbizdenservice/model/Application.java
+++ src/main/java/com/patika/kredinbizdenservice/model/Application.java
@@ -6,34 +6,41 @@ import com.patika.kredinbizdenservice.model.Loan.Loan;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Application {
 
     @Getter private Loan loan;
-    private Product product;
     @Getter private User user;
     @Getter private LocalDateTime localDateTime;
     private ApplicationStatus applicationStatus;
 
-    public Application(Product product, User user, LocalDateTime localDateTime) {
-        this.product = product;
-        this.user = user;
-        this.localDateTime = localDateTime;
-        this.applicationStatus = ApplicationStatus.INITIAL;
-
-        // add application list into user's application list
-        User.getApplicationList().add(this);
-    }
-
-    public Application(Loan loan, User user, LocalDateTime localDateTime) {
+    private Application(Loan loan, User user, LocalDateTime localDateTime) {
         this.loan = loan;
         this.user = user;
         this.localDateTime = localDateTime;
         this.applicationStatus = ApplicationStatus.INITIAL;
 
-        User.addApplication(this);
     }
 
+    public static Application create(Loan loan, User user, LocalDateTime localDateTime) {
+        return new Application(loan, user, localDateTime);
+    }
+
+    public static Application createRandom(Loan loan, User user) {
+
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        LocalDateTime threeMonthsAgo = currentDateTime.minusMonths(3);
+        long minSecond = threeMonthsAgo.atZone(ZoneId.systemDefault()).toEpochSecond();
+        long maxSecond = currentDateTime.atZone(ZoneId.systemDefault()).toEpochSecond();
+        long randomSecond = ThreadLocalRandom.current().nextLong(minSecond, maxSecond);
+        LocalDateTime randomDateTime = LocalDateTime.ofEpochSecond(randomSecond, 0, ZoneOffset.UTC);
+
+        return new Application(loan, user, randomDateTime);
+
+    }
 
 
     @Override
