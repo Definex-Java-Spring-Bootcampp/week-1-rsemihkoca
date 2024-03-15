08e4a77a609864cfb2d091f6e1d5b36356b6c7a8
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-16 01:59:17 +0300

    factory and singleton implemented:src/main/java/com/patika/kredinbizdenservice/model/Campaign.java

--- src/main/java/com/patika/kredinbizdenservice/model/Campaign.java
+++ src/main/java/com/patika/kredinbizdenservice/model/Campaign.java
@@ -15,60 +15,12 @@ public class Campaign {
     private LocalDate updateDate;
     private SectorType sector;
 
-    public Campaign(String title, String content, LocalDate dueDate, LocalDate createDate, LocalDate updateDate, SectorType sector) {
+    private Campaign(String title, String content, LocalDate dueDate, LocalDate createDate, LocalDate updateDate, SectorType sector) {
         this.title = title;
         this.content = content;
         this.dueDate = dueDate;
         this.createDate = createDate;
         this.updateDate = updateDate;
-        this.sector = sector;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public LocalDate getDueDate() {
-        return dueDate;
-    }
-
-    public void setDueDate(LocalDate dueDate) {
-        this.dueDate = dueDate;
-    }
-
-    public LocalDate getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(LocalDate createDate) {
-        this.createDate = createDate;
-    }
-
-    public LocalDate getUpdateDate() {
-        return updateDate;
-    }
-
-    public void setUpdateDate(LocalDate updateDate) {
-        this.updateDate = updateDate;
-    }
-
-    public SectorType getSector() {
-        return sector;
-    }
-
-    public void setSector(SectorType sector) {
         this.sector = sector;
     }
 
