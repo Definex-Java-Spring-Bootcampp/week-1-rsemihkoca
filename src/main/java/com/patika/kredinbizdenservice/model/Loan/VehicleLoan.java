cdec7077ec1c432da72eae8fae7aba847397b878
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-16 01:59:17 +0300

    factory and singleton implemented:src/main/java/com/patika/kredinbizdenservice/model/Loan/VehicleLoan.java

--- src/main/java/com/patika/kredinbizdenservice/model/Loan/VehicleLoan.java
+++ src/main/java/com/patika/kredinbizdenservice/model/Loan/VehicleLoan.java
@@ -13,13 +13,17 @@ public class VehicleLoan extends Loan {
     @Getter private LoanType loanType = LoanType.ARAC_KREDISI;
     private VehicleStatusType VehicleStatusType;
 
-    public VehicleLoan(BigDecimal amount, Integer installment, Double interestRate, Bank bank) {
+    private VehicleLoan(BigDecimal amount, Integer installment, Double interestRate, Bank bank) {
         super(amount, installment, interestRate, bank);
     }
 
     @Override
     void calculate(BigDecimal amount, int installment) {
         //istediği kadar ödeme yapabilir.
+    }
+
+    public static VehicleLoan create(BigDecimal amount, Integer installment, Double interestRate, Bank bank) {
+        return new VehicleLoan(amount, installment, interestRate, bank);
     }
 
     public static VehicleLoan createRandom() {
