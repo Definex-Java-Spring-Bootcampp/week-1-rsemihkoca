19cf379f24e396fceab5610dcaf8a22e12b334f8
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-16 04:14:22 +0300

    reports refactored untill listCreditCardOffersByCampaignCount:src/main/java/com/patika/kredinbizdenservice/factory/CampaignFactory.java

--- src/main/java/com/patika/kredinbizdenservice/factory/CampaignFactory.java
+++ src/main/java/com/patika/kredinbizdenservice/factory/CampaignFactory.java
@@ -1,38 +1,53 @@
 package com.patika.kredinbizdenservice.factory;
 
+import com.patika.kredinbizdenservice.enums.SectorType;
 import com.patika.kredinbizdenservice.model.Bank;
+import com.patika.kredinbizdenservice.model.Campaign;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BankFactory {
+public class CampaignFactory {
 
-    private static volatile BankFactory instance;
-    private List<Bank> bankList = new ArrayList<>();
+    private static volatile CampaignFactory instance;
+    private List<Campaign> campaignList = new ArrayList<>();
 
-    private BankFactory() {
+    private CampaignFactory() {
     }
 
-    public static BankFactory getInstance() {
+    public static CampaignFactory getInstance() {
         if (instance == null) {
-            synchronized (BankFactory.class) {
+            synchronized (CampaignFactory.class) {
                 if (instance == null) {
-                    instance = new BankFactory();
+                    instance = new CampaignFactory();
                 }
             }
         }
         return instance;
     }
 
-    public Bank create(String name, String location) {
-        Bank bank = Bank.create(name, location);
-        bankList.add(bank);
-        return bank;
+    public Campaign create(Bank bank, String title, String content, LocalDate dueDate, LocalDate createDate, LocalDate updateDate, SectorType sector) {
+        Campaign campaign = Campaign.create(bank, title, content, dueDate, createDate, updateDate, sector);
+
+        bank.addCampaign(campaign);
+        campaignList.add(campaign);
+        return campaign;
     }
 
-    public Bank createRandom() {
-        Bank bank = Bank.createRandom();
-        bankList.add(bank);
-        return bank;
+    public Campaign createRandom() {
+        Campaign campaign = Campaign.createRandom();
+
+        campaign.getBank().addCampaign(campaign);
+        campaignList.add(campaign);
+        return campaign;
+    }
+
+    public void createRandomCampaigns(int count) {
+
+        for (int i = 0; i < count; i++) {
+            createRandom();
+        }
+
     }
 }
